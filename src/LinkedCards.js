f4a71cde3afae4666db0c6d24a657e69df3c4c59
Chris Hausner <cthausner@gmail.com>
2021-04-26 20:18:32 -0400

    added cards to link with social

--- src/LinkedCards.js
+++ src/LinkedCards.js
@@ -2,32 +2,51 @@ import React from 'react';
 import styled from 'styled-components';
 import Row from 'react-bootstrap/Row';
 import Card from 'react-bootstrap/Card';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons';
 
+const Icon = styled(FontAwesomeIcon)`
+    font-size: 3rem;
+`;
+
+const Styles = styled.div`
+    .h1 {
+        font-size: 2rem;
+    }
+
+    .card-link {
+        color: #bbb;
+    
+        &:hover {
+          color: black;
+        }
+      }
+`;
 
 export const LinkedCards  = () => (
-    <React.Fragment>
+    <Styles>
         <Row>
             <div class="col-sm-4">
                 <Card>
                     <Card.Body>
-                        <Card.Title>Linkedin</Card.Title>
+                        <Card.Link><Icon href="/" icon={faLinkedinIn} /></Card.Link>
                     </Card.Body>
                 </Card>
             </div>
             <div class="col-sm-4">
                 <Card>
                     <Card.Body>
-                        <Card.Title>GitHub</Card.Title>
+                        <Card.Link><Icon icon={faGithub}/></Card.Link>
                     </Card.Body>
                 </Card>
             </div>
             <div class="col-sm-4">
                 <Card>
                     <Card.Body>
-                        <Card.Title>Projects</Card.Title>
+                        <h1>Projects</h1>
                     </Card.Body>
                 </Card>
             </div>
         </Row>
-    </React.Fragment>
+    </Styles>
 )
